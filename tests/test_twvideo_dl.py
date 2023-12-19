1ace1b3964f05f47446475736dc66e7bc7eba0ac
eggplants <w10776e8w@yahoo.co.jp>
2023-12-20 03:30:24 +0900

    add support for mixed media tweet

--- tests/test_twvideo_dl.py
+++ tests/test_twvideo_dl.py
@@ -3,35 +3,111 @@ from __future__ import annotations
 import pytest
 from freezegun import freeze_time
 
-from twvideo.main import main
-
-TEST_CASES: list[tuple[str, str, str]] = [
-  ('', '', None),
-  ('https://example.com', '', None),
-  ('https://x.com/jack/status/20', '', None),
-  ('https://x.com/jack/status/311512159605649408?s=20', '', None),
-  ('https://x.com/jack/status/284779356075741184?s=20', '', None),
-  ('https://x.com/jack/status/1071575147293769728?s=20', '', None),
-  ('https://x.com/jack/status/630260408666882048?s=20', '', None),
-]
+from twvideo_dl.main import main
 
 TIMESTAMP = "2000-01-02 03:04:05"
 
-@pytest.mark.parametrize("url, out, err", TEST_CASES)
+
+INVALID_TEST_CASES: list[tuple[str, str]] = [
+    ("invalid", "Error: Invalid tweet url.\n"),
+    ("https://example.com", "Error: Invalid tweet url.\n"),
+    (
+        "https://twitter.com/jack/status/20",
+        "Error: Failed to fetch video info. Does this tweet contain of any video?\n",
+    ),
+    (
+        "https://x.com/jack/status/311512159605649408?s=20",
+        "Error: Failed to fetch video info. Does this tweet contain of any video?\n",
+    ),
+    (
+        "https://x.com/jack/status/284779356075741184?s=20",
+        "Error: Failed to fetch video info. Does this tweet contain of any video?\n",
+    ),
+    (
+        "https://x.com/jack/status/1071575147293769728?s=20",
+        "Error: Failed to fetch video info. Does this tweet contain of any video?\n",
+    ),
+]
+
+VALID_TEST_CASES: list[tuple[str, str]] = [
+    (
+        "https://twitter.com/jack/status/630260408666882048?s=20",
+        "Saved: 2000_01_02_03_04_05_000_dX06G7A6camzrcWn.mp4\n",
+    ),
+    (
+        "https://x.com/jack/status/630260408666882048?s=20",
+        "Saved: 2000_01_02_03_04_05_000_dX06G7A6camzrcWn.mp4\n",
+    ),
+    (
+        "https://x.com/Support/status/1577730112853680128",
+        "Saved: 2000_01_02_03_04_05_000_dX06G7A6camzrcWn.mp4\n",
+    ),
+    (
+        "https://x.com/aespa_official/status/1577282815250427907",
+        "Saved: 2000_01_02_03_04_05_000_dX06G7A6camzrcWn.mp4\n",
+    ),
+]
+
+
+@pytest.mark.parametrize(("url", "err"), INVALID_TEST_CASES)
 @freeze_time(TIMESTAMP)
-def test_from_arg(url: str, out: str, err: str, capfd: pytest.CaptureFixture[str], monkeypatch: pytest.MonkeyPatch) -> None:
-    monkeypatch.setattr("sys.argv", lambda _: [__file__, url])
+def test_from_arg_invalid(
+    url: str,
+    err: str,
+    capfd: pytest.CaptureFixture[str],
+    monkeypatch: pytest.MonkeyPatch,
+) -> None:
+    monkeypatch.setattr("sys.argv", [__file__, url])
+    with pytest.raises(SystemExit) as ext:
+        main()
+    captured = capfd.readouterr()
+    assert ext.value.code == 1
+    assert not captured.out
+    assert captured.err == err
+
+
+@pytest.mark.parametrize(("url", "out"), VALID_TEST_CASES)
+@freeze_time(TIMESTAMP)
+def test_from_arg_valid(
+    url: str,
+    out: str,
+    capfd: pytest.CaptureFixture[str],
+    monkeypatch: pytest.MonkeyPatch,
+) -> None:
+    monkeypatch.setattr("sys.argv", [__file__, url])
     main()
     captured = capfd.readouterr()
-    assert captured.out == out  # pyre-fixme[16]
-    assert captured.err == err  # pyre-fixme[16]
+    assert captured.out == out
+    assert not captured.err
 
 
-@pytest.mark.parametrize("url, out, err", TEST_CASES)
+@pytest.mark.parametrize(("url", "err"), INVALID_TEST_CASES[:1])
 @freeze_time(TIMESTAMP)
-def test_from_stdin(url: str, out: str, err: str, capfd: pytest.CaptureFixture[str], monkeypatch: pytest.MonkeyPatch) -> None:
+def test_from_stdin_invalid(
+    url: str,
+    err: str,
+    capfd: pytest.CaptureFixture[str],
+    monkeypatch: pytest.MonkeyPatch,
+) -> None:
+    monkeypatch.setattr("builtins.input", lambda _: url)
+    with pytest.raises(SystemExit) as ext:
+        main()
+    captured = capfd.readouterr()
+    assert ext.value.code == 1
+    assert not captured.out
+    assert captured.err == err
+
+
+@pytest.mark.parametrize(("url", "out"), VALID_TEST_CASES[:1])
+@freeze_time(TIMESTAMP)
+def test_from_stdin_valid(
+    url: str,
+    out: str,
+    capfd: pytest.CaptureFixture[str],
+    monkeypatch: pytest.MonkeyPatch,
+) -> None:
     monkeypatch.setattr("builtins.input", lambda _: url)
     main()
     captured = capfd.readouterr()
     assert captured.out == out
-    assert captured.err == err
+    assert not captured.err
--- twvideo_dl/main.py
+++ twvideo_dl/main.py
@@ -1,13 +1,15 @@
 from __future__ import annotations
 
+import json
 import sys
 import re
 from datetime import datetime
 from urllib.parse import urlparse
 
 import requests
+from nested_lookup import nested_lookup
 
-from typing import TypedDict, NoReturn
+from typing import Any, TypedDict, NoReturn
 
 
 class VideoInfo(TypedDict):
@@ -21,68 +23,132 @@ def exit_with_error(msg: str) -> NoReturn:
     sys.exit(1)
 
 
-def get_video_info_list(video_id: str) -> list[VideoInfo]:
-    headers = {
-        "User-agent": "Mozilla/5.0 (Windows NT 6.3; Win64; x64; rv:76.0) Gecko/20100101 Firefox/76.0",
+def get_session(status_id: str) -> requests.Session:
+    session = requests.Session()
+    session.headers = {
+        "User-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36",
         "accept": ";".join(
             (
                 "text/html,application/xhtml+xml,application/xml",
-                "q=0.9,image/webp,image/apng,*/*",
-                "q=0.8,application/signed-exchange,v=b3;q=0.9",
+                "q=0.9,image/avif,image/webp,image/apng,*/*",
+                "q=0.8,application/signed-exchange;v=b3",
+                "q=0.7",
             )
         ),
         "accept-language": "es-419,es;q=0.9,es-ES;q=0.8,en;q=0.7,en-GB;q=0.6,en-US;q=0.5",
     }
 
-    session = requests.Session()
-
     # Get file contains of bearer token
-    token_request = session.get(
-        f"https://twitter.com/i/videos/tweet/{video_id}", headers=headers
-    )
-    m = re.search('(?<=src=")([^"]+.js)(?=")', token_request.text)
-    if m is None:
-        exit_with_error("Failed to fetch file contains of bearer token")
-    bearer_file = m.group()
+    tweet_req = session.get(f"https://twitter.com/i/videos/tweet/{status_id}")
+    m = re.search('(?<=src=")([^"]+.js)(?=")', tweet_req.text)
+    if not m:
+        exit_with_error("Failed to fetch file contains of bearer token.")
+    token_file_url = m.group()
 
     # Get bearer token
-    file_content = session.get(bearer_file, headers=headers).text
-    m = re.search("Bearer [a-zA-Z0-9%-]+", file_content)
-    if m is None:
-        exit_with_error("Failed to fetch bearer token")
+    token_file_req = session.get(token_file_url)
+    m = re.search("Bearer [a-zA-Z0-9%-]+", token_file_req.text)
+    if not m:
+        exit_with_error("Failed to fetch bearer token.")
     bearer_token = m.group()
-    headers["authorization"] = bearer_token
+    session.headers["authorization"] = bearer_token
 
     # Get guest token
-    guest_token = session.post(
-        "https://api.twitter.com/1.1/guest/activate.json", headers=headers
-    ).json()["guest_token"]
-    headers["x-guest-token"] = guest_token
+    guest_token = (
+        session.post("https://api.twitter.com/1.1/guest/activate.json")
+        .json()
+        .get("guest_token")
+    )
+    if not guest_token:
+        exit_with_error("Failed to fetch guest token.")
+    session.headers["x-guest-token"] = guest_token
+
+    return session
+
+
+def get_video_info_list(status_id: str) -> list[VideoInfo]:
+    session = get_session(status_id)
 
     # Get video links
-    api_request = session.get(
-        f"https://api.twitter.com/1.1/statuses/show.json?id={video_id}", headers=headers
+    api_show_req = session.get(
+        f"https://api.twitter.com/1.1/statuses/show.json?id={status_id}"
     )
-    media = api_request.json().get("extended_entities", {}).get("media", [])
-    videos: list[list[VideoInfo]] = [
-        medium.get("video_info", {}).get("variants", []) for medium in media
+    single_video_info = parse_data(api_show_req.json())
+    if single_video_info:
+        return [single_video_info]
+
+    # Fetch data from GraphQL API if given tweet uses the `mixed media` feature
+    # https://blog.twitter.com/en_us/topics/product/2022/introducing-mixed-media-videos-images-gifs-together-one-tweet
+    multi_video_info = [
+        parse_data(data)
+        for data in nested_lookup("legacy", get_data_from_graphql(status_id, session))
+        if "extended_entities" in data
     ]
-    if len(videos) == 0:
+    if len(multi_video_info) == 0:
         exit_with_error(
             "Failed to fetch video info. Does this tweet contain of any video?"
         )
-    return [
-        sorted(video, key=lambda v: v.get("bitrate", 0))[-1]
-        for video in videos
-        if video != []
-    ]
+    return multi_video_info
+
+
+def get_data_from_graphql(status_id: str, session: requests.Session) -> Any:
+    tweet_html_req = session.get(f"https://twitter.com/i/web/status/{status_id}")
+    m = re.search(
+        r'(?<=href=")https://abs.twimg.com/responsive-web/client-web/main\.[^".]+\.js(?=")',
+        tweet_html_req.text,
+    )
+    if not m:
+        exit_with_error("Failed to fetch web client main js file.")
+    main_js_url = m.group()
+
+    main_js_req = session.get(main_js_url)
+    m = re.search(
+        r'(?<=queryId:")[^"]+(?=",operationName:"TweetDetail")', main_js_req.text
+    )
+    if not m:
+        exit_with_error("Failed to fetch query id.")
+    query_id = m.group()
+    gql_api_url = f"https://twitter.com/i/api/graphql/{query_id}/TweetDetail"
+
+    err_message = session.get(gql_api_url).json().get("errors", [{}])[0].get("message")
+    if not err_message or not isinstance(err_message, str):
+        exit_with_error("Failed to fetch current features.")
+    m = re.search(r"(?<=The following features cannot be null: ).+", err_message)
+    if not m:
+        exit_with_error("Failed to fetch current features.")
+    return session.get(
+        gql_api_url,
+        params={
+            "features": json.dumps(
+                {feature.strip(): True for feature in m.group().split(",")}
+            ),
+            "variables": json.dumps(
+                {
+                    "focalTweetId": status_id,
+                    "includePromotedContent": True,
+                    "withBirdwatchNotes": True,
+                    "withVoice": True,
+                }
+            ),
+        },
+    ).json()
+
+
+def parse_data(data: Any) -> VideoInfo | None:
+    medium = data.get("extended_entities", {}).get("media", [{}])[0]
+    video: list[VideoInfo] = list(medium.get("video_info", {}).get("variants", []))
+    high_quality_video_info = sorted(video, key=lambda v: v.get("bitrate", 0))[-1:]
+    return high_quality_video_info[0] if len(high_quality_video_info) == 1 else None
 
 
 def download_video(video_info: VideoInfo) -> str:
     video_url = video_info["url"]
-    timestamp = datetime.now().strftime("%Y-%m-%d_%H:%M:%S_%s")
+
+    # 0000_11_22_33_44_55_666_aaaBBBcccDDD.mp4
+    timestamp = datetime.now().strftime("%Y_%m_%d_%H_%M_%S_%f")[:-3]
     video_filename = urlparse(video_url).path.split("/")[-1]
     saved_filename = f"{timestamp}_{video_filename}"
+
     # Download Video
     with requests.get(video_url, stream=True) as r:
         with open(saved_filename, "wb") as f:
@@ -93,11 +159,13 @@ def download_video(video_info: VideoInfo) -> str:
 
 
 def main() -> None:
-    video_url = (
+    video_url = urlparse(
         sys.argv[1] if len(sys.argv) == 2 else input("Please input tweet url:\n>>> ")
     )
-    video_id = urlparse(video_url).path.split("/")[-1]
-    for video_info in get_video_info_list(video_id):
+    if not video_url.hostname or video_url.hostname not in ("x.com", "twitter.com"):
+        exit_with_error("Invalid tweet url.")
+    status_id = video_url.path.split("/")[-1]
+    for video_info in get_video_info_list(status_id):
         print("Saved:", download_video(video_info))
 
 
